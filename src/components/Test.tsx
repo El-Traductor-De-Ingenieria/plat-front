9ebd244764bfe2291916dae9ffd8f93d9e3f5246
José Berríos <surphury@gmail.com>
2022-09-13 19:39:06 -0400

    fix: calling API outside of the component

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,11 +1,19 @@
-const res = await fetch('http://localhost:3000/api/auth/status', {
-    credentials: 'include'
-});
+import { useEffect, useState } from 'react';
 
-const data = await res.json();
+const getData = async () => {
+	const res = await fetch('http://localhost:3000/api/auth/status', {
+		credentials: 'include'
+	});
+
+	const data = await res.json();
+
+	return data;
+};
 
 export function Test() {
-    return <div>
-        { JSON.stringify(data) }
-    </div>;
+	const [data, setData] = useState(null);
+	useEffect(() => {
+		getData().then(setData);
+	}, []);
+	return <div>{JSON.stringify(data)}</div>;
 }
