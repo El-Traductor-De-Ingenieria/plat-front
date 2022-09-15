4f962de09ad0022a92ad847b4c411ca77c69f675
UNDESC0N0CID0 <quijadajosed@gmail.com>
2022-09-15 16:30:37 -0400

    Se añadió el componente logo para poder reutilizarlo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,5 @@
+export function Logo() {
+	return <span className="self-center ml-2 text-2xl font-extrabold text-gray-900 whitespace-nowrap dark:text-white">
+    📚 La biblioteca de recursos
+  </span>;
+}
